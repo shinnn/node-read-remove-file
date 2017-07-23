3c565c4b5efd806de0f64bc196e3e5e263c5f3e5
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-07-23 10:40:56 +0900

    validate argument length

--- index.js
+++ index.js
@@ -10,7 +10,17 @@ const {readFile, unlink} = require('graceful-fs');
 const readFileP = promisify(readFile);
 const unlinkP = promisify(unlink);
 
-module.exports = async function readRemoveFile(filePath, options) {
+module.exports = async function readRemoveFile(...args) {
+  const argLen = args.length;
+
+  if (argLen !== 1 && argLen !== 2) {
+    return Promise.reject(new RangeError(`Expected 1 or 2 arguments (path: <string>[, options: Object]), but got ${
+      argLen === 0 ? 'no' : argLen
+    } arguments.`));
+  }
+
+  const [filePath, options] = args;
+
   const data = await readFileP(filePath, options);
   await unlinkP(filePath);
 
--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const writeFileAtomically = require('write-file-atomically');
 const pathExists = require('path-exists');
 
 test('readRemoveFile()', t => {
-  t.plan(5);
+  t.plan(7);
 
   writeFileAtomically('tmp_file0', 'foo')
   .then(() => readRemoveFile('tmp_file0'))
@@ -20,15 +20,31 @@ test('readRemoveFile()', t => {
   writeFileAtomically('tmp_file1', 'bar')
   .then(() => readRemoveFile('tmp_file1', 'utf8'))
   .then(data => {
-    t.strictEqual(data, 'bar', 'should support the second parameter of fs.readFile.');
+    t.equal(data, 'bar', 'should support the second parameter of fs.readFile.');
   })
   .catch(t.fail);
 
   readRemoveFile('this/file/does/not/exist').then(t.fail, err => {
-    t.strictEqual(err.code, 'ENOENT', 'should fail when it cannot read the file.');
+    t.equal(err.code, 'ENOENT', 'should fail when it cannot read the file.');
   }).catch(t.fail);
 
+  readRemoveFile().catch(err => {
+    t.equal(
+      err.toString(),
+      'RangeError: Expected 1 or 2 arguments (path: <string>[, options: Object]), but got no arguments.',
+      'should fail when it takes no arguments.'
+    );
+  });
+
+  readRemoveFile('a', {b: 'c'}, 'd').catch(err => {
+    t.equal(
+      err.toString(),
+      'RangeError: Expected 1 or 2 arguments (path: <string>[, options: Object]), but got 3 arguments.',
+      'should fail when it takes too many arguments.'
+    );
+  });
+
   readRemoveFile({invalid: 'argument'}).then(t.fail, err => {
-    t.strictEqual(err.message, 'path must be a string or Buffer', 'should fail when it takes an invalid argument.');
+    t.equal(err.message, 'path must be a string or Buffer', 'should fail when it takes an invalid argument.');
   }).catch(t.fail);
 });
